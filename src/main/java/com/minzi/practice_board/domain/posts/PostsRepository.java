f52f5bbced52b7f02e7673d0de3d76934b565307
Minji <minjichoe0326@gmail.com>
2023-02-20 22:34:07 +0900

    PostsRepository 생성
    Posts 클래스로 데이터베이스를 접근하게 해줄 리포지토리 생성 (JpaRepository)

---
+++ src/main/java/com/minzi/practice_board/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.minzi.practice_board.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
