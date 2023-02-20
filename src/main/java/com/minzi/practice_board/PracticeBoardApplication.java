7ae91a03aa6c3efeb5bbda84db7a1203e4221788
Minji <minjichoe0326@gmail.com>
2023-02-20 22:00:02 +0900

    init

---
+++ src/main/java/com/minzi/practice_board/PracticeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.minzi.practice_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeBoardApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PracticeBoardApplication.class, args);
+  }
+
+}
