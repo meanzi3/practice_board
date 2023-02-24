cc911c2d1446f81696a1b0cd6282004416031d35
Minji <minjichoe0326@gmail.com>
2023-02-24 20:02:45 +0900

    API 요청을 받을 Controller 작성
    등록, 수정 기능 관련 Controller 작성

---
+++ src/main/java/com/minzi/practice_board/web/PostsApiController.java
@@ -0,0 +1,32 @@
+package com.minzi.practice_board.web;
+
+
+import com.minzi.practice_board.service.posts.PostsService;
+import com.minzi.practice_board.web.dto.PostsSaveRequestDto;
+import com.minzi.practice_board.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor // final이 선언된 모든 필드를 인자값으로 하는 생성자를 생성한다.
+@RestController // Controller 임을 알려주는 표시
+public class PostsApiController {
+  private final PostsService postsService;
+
+  /**
+   * 등록
+   */
+  @PostMapping("/api/v1/posts") // POST 통신 (함께 받아야 하는 데이터가 있으면, @RequestBody 활용)
+  public Long save(@RequestBody PostsSaveRequestDto requestDto){
+    return postsService.save(requestDto);
+  }
+  // @RequestBody를 이용해 HTTP Body에 담긴 데이터를 매핑하여 가지고 온다. 받은 데이터를 service에서 save
+
+  /**
+   * 수정
+   */
+  @PutMapping("/api/v1/posts/{id}")
+  public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+    return postsService.update(id, requestDto);
+  }
+
+}
