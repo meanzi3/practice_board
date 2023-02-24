54369dd3cf7269ccba050b6d12783b45255a089f
Minji <minjichoe0326@gmail.com>
2023-02-24 18:50:45 +0900

    PostsRepositoryTest 생성
    게시글이 저장되고 불러올 수 있는지 테스트코드를 작성하여 확인하였다.

---
+++ src/test/java/com/minzi/practice_board/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.minzi.practice_board.domain.posts;
+
+import com.minzi.practice_board.domain.posts.Posts;
+import com.minzi.practice_board.domain.posts.PostsRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest // 별다른 설정 없이 SpringBootTest 사용 시 H2 데이터베이스를 자동 실행
+public class PostsRepositoryTest {
+
+  @Autowired
+  PostsRepository postsRepository;
+
+  @After // JUnit에서 단위 테스트가 끝날 때마다 수행되는 메소드
+  public void cleanup() {
+    postsRepository.deleteAll();
+  }
+
+  @Test
+  public void 게시글저장_불러오기(){
+    // given
+    String title = "테스트 게시글";
+    String content = "테스트 본문";
+
+    postsRepository.save(Posts.builder()
+            .title(title)
+            .content(content)
+            .author("aaa@gmail.com")
+            .build()); // posts에 insert(id값이 있으면)/update(id값이 없으면 쿼리를 실행한다.
+
+    // when
+    List<Posts> postsList = postsRepository.findAll(); // posts에 있는 모든 데이터를 조회해 list로 저장
+
+    // then
+    Posts posts = postsList.get(0);
+    assertThat(posts.getTitle()).isEqualTo(title);
+    assertThat(posts.getContent()).isEqualTo(content);
+  }
+}
