a863d6d5b2d6b017a8c5455203f6a14492c6543d
Minji <minjichoe0326@gmail.com>
2023-02-24 20:35:07 +0900

    BaseTimeEntity를 상속하도록 수정
    BaseTimeEntity를 상속받아, 엔티티가 생성되고 수정되는 날짜를 자동 저장하도록 한다.

--- src/main/java/com/minzi/practice_board/domain/posts/Posts.java
+++ src/main/java/com/minzi/practice_board/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.minzi.practice_board.domain.posts;
 
+import com.minzi.practice_board.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor // 기본 생성자 자동 추가
 @Entity // 테이블과 링크될 클래스임을 나타낸다.
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
   @Id // 해당 테이블의 PK(Primary Key) 필드를 나타낸다.
   @GeneratedValue(strategy = GenerationType.IDENTITY)
