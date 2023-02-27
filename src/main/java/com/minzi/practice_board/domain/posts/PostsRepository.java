b7f28d4db159afbdd744829959b3f5bb80e40278
Minji <minjichoe0326@gmail.com>
2023-02-27 20:39:04 +0900

    리포지토리에 쿼리 작성
    리포지토리에 저장된 posts 객체를 id 값 기준 내림차순으로 정렬하여 리스트로 저장하도록 쿼리를 작성하였다.

--- src/main/java/com/minzi/practice_board/domain/posts/PostsRepository.java
+++ src/main/java/com/minzi/practice_board/domain/posts/PostsRepository.java
@@ -1,6 +1,13 @@
 package com.minzi.practice_board.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+  @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+  List<Posts> findAllDesc();
+  // posts 객체를 id 값 기준으로 내림차순으로 정렬하여 findAllDesc 리스트로 저장
 }
